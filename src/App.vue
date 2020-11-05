fcadc308cd5898c2c86b3828bd343f2f8beaeeca
ErikESanchez <eriksanchez7247@gmail.com>
2020-11-05 06:18:55 -0600

    Have the Vue instance running and weather api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="What's up"/>
+  <button @click="getWeatherData">What Up</button>
+  <button @click="bruh"></button>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+// Todo: Figure out how to use vuex later
+// import { createStore } from "vuex";
+import axios from "axios";
+// import apikey from "./apikey";
+const weatherURL = "http://api.weatherapi.com/v1/forecast.json";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  methods: {
+    async getWeatherData() {
+      console.log("bruh");
+      return await axios
+        .get(weatherURL, {
+          params: {
+            key: "d56a149322664e6eb25114019200311",
+            q: "Dallas",
+            days: 5
+          },
+        })
+        .then((res) => {
+          console.log(res);
+        });
+    },
+  },
+};
 </script>
 
 <style>
